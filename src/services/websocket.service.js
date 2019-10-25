b3789b391e12b2a0ec097cccab6ce51e452f905e
York Gong <york.gong@trumpf.com>
2019-10-25 10:35:15 +0800

    import environment variables in project

--- src/services/websocket.service.js
+++ src/services/websocket.service.js
@@ -1,6 +1,48 @@
-import React from 'react';
+import { openWebsocket, onMessageWebSocket, closeWebsocket } from '../store/actions/app';
 
-class WebsocketService extends React.Component{
-   static ws = new WebSocket();    
+export function ws_connect(data, dispatch) {
+   const ws = new WebSocket(`${process.env.REACT_APP_WEBSOCKET_URL}`);
+   const machineList = data;
+   let macUuids = [];
+   const collectUuid = (arr) => {
+       let macUuids = [];
+       for(let i=0; i< arr.length; i++){
+           if(arr[i].Machines && Array.isArray(arr[i].Machines))
+           {
+               macUuids = macUuids.concat(collectUuid(arr[i].Machines))
+           }
+           else {
+               macUuids.push(arr[i].MachineUuid)
+           }
+       }
+       return macUuids;
+   }
+   
+   ws.onopen = () => {
+       // send machine uuids, otherwise no data back in message
+       // relative to backend 
+       macUuids = collectUuid(machineList);
 
+       if(macUuids.length > 0)
+       {
+           ws.send(JSON.stringify(macUuids));
+           dispatch(openWebsocket());
+           console.log(`ws connected`);
+       }else{
+           dispatch(closeWebsocket());
+           console.log(`Ws didn't received macUuids.`)
+       }
+   }
+
+   ws.onmessage = (evt) => {
+       if(evt) {
+           dispatch(onMessageWebSocket(JSON.parse(evt.data)));
+       }
+   }
+
+   ws.onclose = (e) => {
+       dispatch(closeWebsocket());
+       console.log(`ws closed`);
+   }
 }
+
--- src/store/actions/app.js
+++ src/store/actions/app.js
@@ -1,4 +1,5 @@
-import { REQUEST_MACHINELIST_SUCCESS, REQUEST_MACHINELIST_PENDING, REQUEST_MACHINELIST_ERROR, WEBSOCKET_OPEN, WEBSOCKET_MESSAGING, WEBSOCKET_CLOSE } from '../action-types';
+import { REQUEST_MACHINELIST_SUCCESS, REQUEST_MACHINELIST_ERROR, WEBSOCKET_OPEN, WEBSOCKET_MESSAGING, WEBSOCKET_CLOSE } from '../action-types';
+import { ws_connect } from '../../services/websocket.service';
 import axios from 'axios';
 
 export function requestMachineListSuccess(data){
@@ -43,12 +44,12 @@ export function closeWebsocket(){
 
 export function requestMachineList(){
      return (dispatch, getState) => {
-         axios.get(`https://localhost:44395/dt/api/MS/GetAllMachines`)
+         axios.get(`${process.env.REACT_APP_API_URL}/MS/GetAllMachines`)
            .then(
                response => {
                    console.log(response.request.status? response.request.status : 'response unknown');
                    dispatch(requestMachineListSuccess(response.data));
-                   connectWs(response.data, dispatch);
+                   ws_connect(response.data, dispatch);
                }
            ).catch(
                error => {
@@ -56,51 +57,5 @@ export function requestMachineList(){
                }
            )
      }
-}
-
-function connectWs(data, dispatch){
-    const ws = new WebSocket('wss://localhost:44395/dt/wscall');
-    const machineList = data;
-    let macUuids = [];
-    const collectUuid = (arr) => {
-        let macUuids = [];
-        for(let i=0; i< arr.length; i++){
-            if(arr[i].Machines && Array.isArray(arr[i].Machines))
-            {
-                macUuids = macUuids.concat(collectUuid(arr[i].Machines))
-            }
-            else {
-                macUuids.push(arr[i].MachineUuid)
-            }
-        }
-        return macUuids;
-    }
-    
-    ws.onopen = () => {
-        // send machine uuids, otherwise no data back in message
-        // relative to backend 
-        macUuids = collectUuid(machineList);
-
-        if(macUuids.length > 0)
-        {
-            ws.send(JSON.stringify(macUuids));
-            dispatch(openWebsocket());
-            console.log(`ws connected`);
-        }else{
-            dispatch(closeWebsocket());
-            console.log(`Ws didn't received macUuids.`)
-        }
-    }
-
-    ws.onmessage = (evt) => {
-        if(evt) {
-            dispatch(onMessageWebSocket(JSON.parse(evt.data)));
-        }
-    }
-
-    ws.onclose = (e) => {
-        dispatch(closeWebsocket());
-        console.log(`ws closed`);
-    }
 }
 
